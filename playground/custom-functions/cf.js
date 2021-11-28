4a978c02bc78a06a020a23d4d3c0ca80bb90d918
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-11-28 11:26:57 -0600

    parameters and arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,12 +1,33 @@
 //function definition
-function calculateBill() {
-  //   console.log("running calculate bill");
-  const total = 100 * 1.13;
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
+  // console.log(billAmount, taxRate);
+  const total =
+    billAmount + billAmount * taxRate * taxRate + billAmount * tipRate;
   return total;
 }
 
 // function call / running the function
-// const myTotal = calculateBill();
+const myTotal = calculateBill(100, 0.13);
 // console.log(`Your total is ${myTotal}`);
 
-console.log(`Your total is $${calculateBill()}`);
+// console.log(myTotal);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo("Wes");
+// console.log(greeting);
+
+// const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15); // this works - it will run the expression first
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = "Silly Goose") {
+  return `HEY ${name.toUpperCase()}!`;
+}
+
+const myBill4 = calculateBill(100, 0.13, 0.15);
+console.log(myBill4);
