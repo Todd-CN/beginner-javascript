71013c8010523f40adb23518f173de776e14ac7f
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-12-12 11:59:52 -0600

    debugging

--- exercises/16 - Debugging/debugging-START.js
+++ exercises/16 - Debugging/debugging-START.js
@@ -1,12 +1,14 @@
 const people = [
-  { name: 'Wes', cool: true, country: 'Canada' },
-  { name: 'Scott', cool: true, country: 'Merica' },
-  { name: 'Snickers', cool: false, country: 'Dog Country' },
+  { name: "Wes", cool: true, country: "Canada" },
+  { name: "Scott", cool: true, country: "Merica" },
+  { name: "Snickers", cool: false, country: "Dog Country" },
 ];
 
 people.forEach((person, index) => {
   console.log(person.name);
 });
+
+console.table(people);
 
 // Console Methods
 
@@ -24,22 +26,31 @@ people.forEach((person, index) => {
 
 // Some Setup Code
 
+function doALotOfStuff() {
+  console.group("Doing some stuff");
+  console.log("Hey Im one");
+  console.warn("watch out!");
+  console.error("hey");
+  console.groupEnd("Doing some stuff");
+}
+
 function doctorize(name) {
+  // console.count(`running Doctorize for ${name}`);
   return `Dr. ${name}`;
 }
 
 function greet(name) {
-  doesntExist();
+  doesntExist(); // will cause an error
   return `Hello ${name}`;
 }
 
 function go() {
-  const name = doctorize(greet('Wes'));
+  const name = doctorize(greet("Wes"));
   console.log(name);
 }
 
-const button = document.querySelector('.bigger');
-button.addEventListener('click', function(e) {
+const button = document.querySelector(".bigger");
+button.addEventListener("click", function (e) {
   const newFontSize =
     parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
   e.currentTarget.style.fontSize = `${newFontSize}px`;
@@ -47,9 +58,9 @@ button.addEventListener('click', function(e) {
 
 // A Dad joke fetch
 async function fetchDadJoke() {
-  const res = await fetch('https://icanhazdadjoke.com/', {
+  const res = await fetch("https://icanhazdadjoke.com/", {
     headers: {
-      Accept: 'text/plain',
+      Accept: "text/plain",
     },
   });
   const joke = await res.text();
