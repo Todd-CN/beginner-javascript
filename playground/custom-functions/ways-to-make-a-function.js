7dce4b8f226220a3b1fec77c0735430e583ee202
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-12-12 11:09:18 -0600

    declaring functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -59,10 +59,59 @@ const add = (a, b = 3) => a + b;
 function makeABaby(first, last) {
   const baby = {
     name: `${first} ${last}`,
+    age: 0,
   };
   return baby;
 }
 
 const makeAnotherBaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
 
-// left off at 23:06
+// IIFE - immediately invoked function expression
+(function (age) {
+  console.log("Running the Anonymous Function");
+  return `You are cool and age ${age}`;
+})(10);
+
+// You wont use these often, but they are great for when you need to run a function immediately, create a closure, create a private scope, or create a module
+
+// Methods!
+// * methods are functions that are attached to an object, they live inside the object
+// 3 different ways to write methods
+const wes = {
+  name: "Wes Bos",
+  sayHi: function () {
+    console.log(this); // this is going to be equal the object that it was called against ('wes' is the object in this case)
+    console.log("Hey Wes!");
+    return "Hey Wes";
+  },
+  // Short hand Method
+  yellHi() {
+    console.log("Hey Wesssss!");
+    // this.sayHi()
+  },
+  // Arrow function
+  whisperHi: () => {
+    console.log("hi wes im a mouse");
+  },
+};
+
+// Callback functions
+// something that will happen when something else is done
+// click callback
+const button = document.querySelector(".clickMe");
+
+button.addEventListener("click", wes.yellHi);
+// notice we're not running it here () we're just saying hey browser, here's a function to call when somebody clicks that button
+
+function handleClick() {
+  console.log("Great Clicking!!");
+}
+
+button.addEventListener("click", function () {
+  console.log("NIce Job!!!");
+});
+
+// Timer Callback
+setTimeout(() => {
+  console.log("DONE! Time to eat!");
+}, 1000);
